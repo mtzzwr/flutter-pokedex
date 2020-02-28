2662108bf9cb81d428d9b65b711d4ab7ad071034
mtzzwr <mateus.gsep@gmail.com>
2020-02-28 17:56:49 -0300

    splashscreen adicionada

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/pages/homepage/home_page.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'consts/consts_app.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,8 +16,52 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return _introScreen();
+  }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 5,
+        gradientBackground: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Color(0xffED213A), Color(0xff93291E)],
+        ),
+        navigateAfterSeconds: HomePage(),
+        loaderColor: Colors.transparent,
+      ),
+      Padding(
+        padding: const EdgeInsets.only(left: 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset(ConstsApp.whitePokeball),
+              height: 240,
+              width: 240,
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
